f77080cf99fe8da32df6934be2483b1d540c792a
William Castelli <billycastelli@gmail.com>
2017-10-09 20:03:11 -0700

    day2

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/SearchBar';
 
 const App = () => {
 	return (
 		<div>
-			<h1>This is a test</h1>
-			<p>Hello world hello world</p>
+			<SearchBar /> 
 		</div>
 		)
 }
